416ae453977ed7c2ee1b126a53c352795980dff8
Pedrosntss <pedro.henrique.phst112@gmail.com>
2026-04-08 17:20:45 -0300

    Add files via upload

---
+++ AulaPython/exe 8 contador regressivo - Copia.py
@@ -0,0 +1,9 @@
+numero_limite = int(input("Digite um número para contar até ele: "))
+
+contador = numero_limite
+
+while contador >= -0 : 
+    print(contador)
+    contador -= 1
+
+print ("contagem finalizada!")
---
+++ AulaPython/exe contador progressivo.py
@@ -0,0 +1,8 @@
+numero_limite = int(input("Digite um número para contar até ele: "))
+
+contador = 1
+
+while contador <= numero_limite : 
+    print(contador)
+    contador += 1
+print ("contagem finalizada!")
